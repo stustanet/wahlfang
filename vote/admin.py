fe49e29a31a4ba9cf71d2d20c8191a9c18d29d3a
Julien Schmidt <js@stusta.net>
2020-04-23 13:31:01 +0200

    make vote models available in admin interface

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Election
+from .models import Application
+from .models import User
+
+admin.site.register(Election)
+admin.site.register(Application)
+admin.site.register(User)

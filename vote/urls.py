b062ca9b73485294db706507ccb7d684fba7b8ae
Julien Schmidt <js@stusta.net>
2020-04-26 10:36:26 +0200

    improved urls

--- vote/urls.py
+++ vote/urls.py
@@ -1,24 +1,29 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 from vote import forms
 from vote import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vote', views.vote, name='vote'),
-    path('upload_application', views.upload_application, name='upload-application'),
-    path('view_application/<pk>', views.view_application, name='view-application')
-]
 
-# account management stuff
-urlpatterns += [
+    # code login
     path('code', auth_views.LoginView.as_view(
         authentication_form=forms.AccessCodeAuthenticationForm,
         template_name='vote/login.html'
     ), name='code_login'),
+    path('code/', RedirectView.as_view(pattern_name='code_login')),
     path('code/<str:access_code>', views.code_login),
     path('logout', auth_views.LogoutView.as_view(
         next_page='index',
-    ), name='logout')
+    ), name='logout'),
+
+    # voting
+    path('vote', views.vote, name='vote'),
+
+    # applications
+    path('application', views.upload_application, name='upload_application'),
+    path('application/', RedirectView.as_view(pattern_name='upload_application')),
+    path('application/<pk>', views.view_application, name='view_application')
 ]

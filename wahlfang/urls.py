947ce4353c50e0a1a60873de6e7372847b3017a0
Michael Loipführer <michael.loipfuehrer@stusta.de>
2020-04-29 16:49:14 +0200

    disable django admin in non-debug

--- wahlfang/urls.py
+++ wahlfang/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('vote.urls', namespace='vote')),
     path('management/', include('management.urls', namespace='management'))
 ]
 
 if settings.DEBUG:
+    from django.contrib import admin
+
+    urlpatterns += path('admin/', admin.site.urls),
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

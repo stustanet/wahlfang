c0e3c80bfe4a143a15814471c836f8575c7b14b3
Julien Schmidt <js@stusta.net>
2020-04-30 13:37:57 +0200

    show applicants in random order

--- vote/templatetags/vote_extras.py
+++ vote/templatetags/vote_extras.py
@@ -1,7 +1,17 @@
+import random
+
 from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def applicant_name(application):
     return application.get_display_name()
+
+
+@register.filter
+def shuffle(items):
+    items = list(items)[:]
+    random.shuffle(items)
+    return items
